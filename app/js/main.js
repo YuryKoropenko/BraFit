69446e32b344cac535a7b74b25725fb3d21db6f1
YuryKoropenko <yurykoropenko@gmail.com>
2017-12-18 17:11:34 +0200

    адаптив для главной + страница возврат и обмен

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,17 @@ $(function() {
 		items:1
 	});
 
+	/*mob-menu*/
+	$('.h-mobmenu').on('click', function() {
+		$('.header__bottom').toggle();
+		$(document).click(function(event) {
+			if ($(event.target).closest('.header__bottom').length) return;
+				$('.header__bottom').hide();
+				event.stopPropagation();
+			});
+		return false;
+	});
+
+	$('.p-filter__inst').fancybox();
+
 });
